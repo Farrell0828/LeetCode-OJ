d2ba9e11b077bc061309e73adabc2420fe3ae674
Farrell Meng <mengfanqi0828@163.com>
2018-01-11 11:19:09 +0800

    新增#461问题及解答。

---
+++ NO.461 Hamming Distance/461.cpp
@@ -0,0 +1,46 @@
+/**
+ * 461.cpp -- #461 of LeetCode OJ
+ *
+ * Source: https://leetcode.com/problems/hamming-distance/description/
+ * Author: Farrell Meng
+ * Date:   2018-01-11
+ *
+ * The Hamming distance between two integers is the number of positions at
+ * which the corresponding bits are different.
+ * Given two integers x and y, calculate the Hamming distance.
+ *
+ * Note: 0 ¡Ü x, y < 2^31.
+ *
+ * Example:
+ * Input: x = 1, y = 4
+ * Output: 2
+ *
+ * Explanation:
+ *   1  (0 0 0 1)
+ *   4  (0 1 0 0)
+ *         ¡ü   ¡ü
+ *
+ * The above arrows point to positions where the corresponding bits are different.
+ */
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	int hammingDistance(int x, int y) {
+		int z = x ^ y;
+		int count = 0;
+		while (z) {
+			if (z & 1) count++;
+			z = z >> 1;
+		}
+		return count;
+	}
+};
+
+int main() {
+	Solution s;
+	int x = 1, y = 4;
+	cout << s.hammingDistance(x, y) << endl;
+}
